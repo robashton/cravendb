233539d306ec96ec1a239a9ee9bfa9897eadc2c2
Rob Ashton <robashton@codeofrob.com>
2013-09-07 14:34:14 +0100

    can delete an object

--- src/cravendb/storage.clj
+++ src/cravendb/storage.clj
@@ -7,14 +7,19 @@
 
 (defprotocol Storage
   (store-blob [this id data])
+  (delete-blob [this id])
   (get-blob [this id]))
 
 (defrecord LevelStorage [records-to-remove records-to-put]
   Storage
   (store-blob [this id data]
     (assoc-in this [:records-to-put id] data))
+  (delete-blob [this id]
+    (assoc-in this [:records-to-remove id] true))
   (get-blob [this id]
-    (get records-to-put id)))
+    (if (get records-to-remove id)
+      nil
+      (get records-to-put id))))
 
 #_ (def mymap {})
 #_ (get (assoc mymap "id" 2) )
@@ -29,6 +34,12 @@
 ;; Putting and retrieving an object
 #_(-> storage
     (.store-blob "1" "hello")
+    (.get-blob "1"))
+
+;; Deleting an object that exists
+#_(-> storage
+    (.store-blob "1" "hello")
+    (.delete-blob "1")
     (.get-blob "1"))
 
 
