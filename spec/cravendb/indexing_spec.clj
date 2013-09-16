3116b4b78decea4f74d5e8ac1c6f38546372f136
Rob Ashton <robashton@codeofrob.com>
2013-09-16 22:45:22 +0100

    proved that data has indeed ended up in the index

--- spec/cravendb/indexing_spec.clj
+++ spec/cravendb/indexing_spec.clj
@@ -7,9 +7,8 @@
             [cravendb.storage :as storage]
             [cravendb.lucene :as lucene]))
 
-(def test-index {:id "test" :map (fn [doc] {"author" (doc :author)}) :storage (lucene/create-memory-index) })
-
-(def test-indexes [ test-index ])
+(defn create-test-index []  {:id "test" :map (fn [doc] {"author" (doc :author)}) :storage (lucene/create-memory-index) })
+(defn create-test-indexes [] [ (create-test-index) ])
 
 (def write-three-documents 
   (fn [db]
@@ -28,36 +27,45 @@
         (.commit!)))))
 
 (describe "indexing some documents"
+  (with test-indexes (create-test-indexes))
   (it "will index all the documents"
     (with-db (fn [db]
         (write-three-documents db)
-        (indexing/index-documents! db test-indexes)
+        (indexing/index-documents! db @test-indexes)
         (with-open [tx (.ensure-transaction db)]
           (should= 3 (indexing/last-index-doc-count tx))
           (should= (docs/last-etag tx) (indexing/last-indexed-etag tx)))))))
 
 (describe "indexing new documents"
+  (with test-indexes (create-test-indexes))
   (it "will index only the new documents"
     (with-db (fn [db]
         (write-three-documents db)
-        (indexing/index-documents! db test-indexes)
+        (indexing/index-documents! db @test-indexes)
         (write-one-document db)
-        (indexing/index-documents! db test-indexes)
+        (indexing/index-documents! db @test-indexes)
         (with-open [tx (.ensure-transaction db)]
           (should= (docs/last-etag tx) (indexing/last-indexed-etag tx))
           (should= 1 (indexing/last-index-doc-count tx)))))))
 
-
-;; Bugger, I need to do this the Couch way
-;; invoke a map function for every value I want to return
-;; Or allow lists
-;; 
 (describe "loading indexes from the database and indexing using them"
+  (with test-indexes (create-test-indexes))
   (it "will index all the documents"
     (with-db (fn [db]
         (write-three-documents db)
+        (indexing/index-documents! db @test-indexes)
+        (with-open [reader (.open-reader ((first @test-indexes) :storage))]
+            (should== '("2") (.query reader { :query "author:vicky"})))))))
+
+(describe "querying an index with content in it"
+  (with test-indexes (create-test-indexes))
+  (it "will return the right document ids"
+    (with-db (fn [db]
+        (write-three-documents db)
         (with-open [tx (.ensure-transaction db)]
-          (.commit! (indexes/put-index tx { :id "by_author" :map "(fn [doc] {\"author\" (doc :author)})"})))
+          (.commit! 
+            (indexes/put-index tx 
+                { :id "by_author" :map "(fn [doc] {\"author\" (doc :author)})"})))
         (indexing/index-documents! db (indexes/load-compiled-indexes db))
         (with-open [tx (.ensure-transaction db)]
           (should= 4 (indexing/last-index-doc-count tx)) ;; The index counts
