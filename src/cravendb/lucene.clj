2538488af9a7735c9f72cae969dcf7c6b37ac342
Rob Ashton <robashton@codeofrob.com>
2013-09-16 19:15:34 +0100

    Working out the right level of abstraction over Lucene (as little as possible I think)

--- src/cravendb/lucene.clj
+++ src/cravendb/lucene.clj
@@ -5,53 +5,133 @@
            (org.apache.lucene.store RAMDirectory)
            (org.apache.lucene.util Version)
            (org.apache.lucene.index IndexWriterConfig)
+           (org.apache.lucene.index IndexWriter)
+           (org.apache.lucene.index DirectoryReader)
+           (org.apache.lucene.search IndexSearcher)
+           (org.apache.lucene.queryparser.classic QueryParser)
+           (org.apache.lucene.document Document)
+           (org.apache.lucene.document Field)
+           (org.apache.lucene.document TextField)
            (java.util Collection Random)
            (java.io File File PushbackReader IOException FileNotFoundException )))
 
-(defprotocol IndexingSession
-  (add-document! [this fields]) 
+;; Question: Is there a default one of these?
+(defprotocol Closeable
   (close [this]))
 
-(defrecord LuceneIndexing [analyzer directory config]
-  IndexingSession
-  (add-document! [this fields])
-  (close [this]))
+(defprotocol IndexStore
+  (open-writer [this])
+  (open-reader [this]))
 
-(defn create-index []
-  (let [
-        analyzer (StandardAnalyzer. Version/LUCENE_CURRENT)
+(defprotocol IndexWriting
+   (put-entry [this ref-id content])
+   (commit! [this]))
+
+(defprotocol IndexReading
+  (query [this options]))
+
+(defrecord LuceneIndexReading [reader]
+  IndexReading
+  Closeable
+  (query [this options]
+    (let [searcher (IndexSearcher. reader)
+          parser (QueryParser. Version/LUCENE_CURRENT "" analyzer) 
+          query (.parse parser (options :query))
+          result (.search searcher query nil 1000)
+          scoredocs (.scoreDocs result)
+          docs (for [x (range 0 (count scoredocs))] 
+                  (.doc searcher (.doc (aget scoredocs x))))]
+          (map (fn [d] (.get d "__document_id")) docs)))
+  (close [this]
+    (.close reader)))
+
+(defrecord LuceneIndexWriting [writer]
+  IndexWriting
+  Closeable
+  (put-entry [this ref-id content]
+    (let [doc (Document.)
+          fields (for [[k v] content] 
+                   (do
+                     (println k v)
+                     (Field. (pr-str k) (pr-str v) TextField/TYPE_STORED)))]
+      (doseq [f fields] (.add doc f))
+      (.add doc (Field. "__document_id" ref-id TextField/TYPE_STORED))
+      (.addDocument writer doc))
+      this)
+   (commit! [this] 
+     (.commit writer)
+     this)
+   (close [this]
+     (.close writer)))
+
+(defrecord LuceneIndex [analyzer directory config]
+  IndexStore
+  Closeable
+  (open-writer [this] (LuceneIndexWriting. (IndexWriter. directory config)))
+  (open-reader [this] (LuceneIndexReading. (DirectoryReader/open directory)))
+  (close [this]
+    (.close writer)
+    (.close directory)))
+
+(defn create-index [index-path]
+  (let [analyzer (StandardAnalyzer. Version/LUCENE_CURRENT)
         directory (RAMDirectory.)
-        config (IndexWriterConfig. Version/LUCENE_CURRENT analyzer)]))
+        config (IndexWriterConfig. Version/LUCENE_CURRENT analyzer)]
+    (LuceneIndex. analyzer directory config)))
 
-(create-index)
+#_ (def index (create-index "foo"))
+#_ (.close index)
 
+#_ (with-open [writer (.open-writer index)]
+      (-> writer
+        (.put-entry "1" { "foo" "blah"})
+        (.put-entry "2" { "foo" "cray"})
+        (.put-entry "3" { "foo" "sneak"})
+        (.put-entry "4" { "foo" "vlga"})
+        (.put-entry "5" { "foo" "vlga"})
+        (.put-entry "6" { "foo" "vlga"})
+        (.put-entry "7" { "foo" "vlga"})
+        (.put-entry "8" { "foo" "vlga"})
+        (.commit!)
+        (.close)))
 
-;;    Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_CURRENT);
-;;
-;;    // Store the index in memory:
-;;    Directory directory = new RAMDirectory();
-;;    // To store an index on disk, use this instead:
-;;    //Directory directory = FSDirectory.open("/tmp/testindex");
-;;    IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_CURRENT, analyzer);
-;;    IndexWriter iwriter = new IndexWriter(directory, config);
-;;    Document doc = new Document();
-;;    String text = "This is the text to be indexed.";
-;;    doc.add(new Field("fieldname", text, TextField.TYPE_STORED));
-;;    iwriter.addDocument(doc);
-;;    iwriter.close();
-;;    
-;;    // Now search the index:
-;;    DirectoryReader ireader = DirectoryReader.open(directory);
-;;    IndexSearcher isearcher = new IndexSearcher(ireader);
-;;    // Parse a simple query that searches for "text":
-;;    QueryParser parser = new QueryParser(Version.LUCENE_CURRENT, "fieldname", analyzer);
-;;    Query query = parser.parse("text");
-;;    ScoreDoc[] hits = isearcher.search(query, null, 1000).scoreDocs;
-;;    assertEquals(1, hits.length);
-;;    // Iterate through the results:
-;;    for  int i = 0; i < hits.length; i++) {
-;;      Document hitDoc = isearcher.doc(hits[i].doc);
-;;      assertEquals("This is the text to be indexed.", hitDoc.get("fieldname"));
-;;     
-;;    ireader.close();
-;;    directory.close();
+#_ (with-open [reader (.open-reader index)]
+      (doall (.query reader { :query "foo:[vlga]"})))
+
+;; ;; Putting stuff into the index
+;; #_ (def analyzer (StandardAnalyzer. Version/LUCENE_CURRENT))
+;; #_ (def directory (RAMDirectory.))
+;; #_ (.close directory)
+;; #_ (def config (IndexWriterConfig. Version/LUCENE_CURRENT analyzer))
+;; #_ (def writer (IndexWriter. directory config))
+;; #_ (.close writer)
+;; #_ 
+;; #_ (def doc (Document.))
+;; #_ (.add doc (Field. "foo" "searchtext", TextField/TYPE_STORED))
+;; #_ (.add doc (Field. "__document_id" "1", TextField/TYPE_STORED))
+;; #_ (.addDocument writer doc)
+;; #_ 
+;; #_ 
+;; #_ ;; Querying the index
+;; #_ (def reader (DirectoryReader/open directory))
+;; #_ (.close reader)
+;; #_ (def searcher (IndexSearcher. reader))
+;; #_ 
+;; #_ (def parser (QueryParser. Version/LUCENE_CURRENT "" analyzer))
+;; #_ (def query (.parse parser "foo:searchtext"))
+;; #_ 
+;; #_ (def result (.search searcher query nil 1000))
+;; #_ (count (.scoreDocs result))
+;; 
+;; 
+;; 
+;; #_ 
+;; #_ (def indexed-documents (for [x (range 0 (count (.scoreDocs result)))] 
+;;   (.doc searcher (.doc (aget (.scoreDocs result) x)))))
+;; #_ 
+;; #_ (def document-ids (map (fn [d] (.get d "__document_id")) indexed-documents))
+;; #_ 
+;; #_ (doall document-ids)
+;; 
+;; 
+;; 
