8cbc87885c5c6d7e28dc08d51b73a2669c8cff18
Rob Ashton <robashton@codeofrob.com>
2013-09-23 19:18:26 +0100

    Added a scratch-pad for index catch-up

--- src/cravendb/scratch.clj
+++ src/cravendb/scratch.clj
@@ -13,6 +13,39 @@
             [cravendb.lucene :as lucene])
   (use [cravendb.testing]))
 
+#_ (do
+   (with-open [db (indexengine/start 
+                   (storage/create-storage "testdb"))]
+    (try
+      (with-open [tx (.ensure-transaction db)]
+        (-> tx
+          (docs/store-document "1" (pr-str { :foo "bar" }) ) 
+          (docs/store-document "2" (pr-str { :foo "bas" }) ) 
+          (docs/store-document "3" (pr-str { :foo "baz" }) )
+          (.commit!)))
+
+    (with-open [tx (.ensure-transaction db)]
+      (-> tx
+          (indexes/put-index { :id "by_bar" :map "(fn [doc] {\"foo\" (:foo doc)})"})   
+          (.commit!))) 
+      
+    (println "waiting for indexing")
+    (indexing/wait-for-index-catch-up db)
+     (println "waited for indexing, putting index")
+  
+    (with-open [tx (.ensure-transaction db)]
+      (-> tx
+          (indexes/put-index { :id "by_foo" :map "(fn [doc] {\"foo\" (:foo doc)})"})   
+          (.commit!))) 
+
+    (println "put index, querying against it")
+    (println (query/execute db (indexengine/get-engine db) { :index "by_foo" :query "foo:bar" :wait true} )) 
+
+    (finally
+      (indexengine/teardown db))))
+    (fs/delete-dir "testdb"))
+
+
 #_ (client/put-index 
     "http://localhost:8080" 
     "by_username" 
