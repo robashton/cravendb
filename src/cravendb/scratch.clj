5c3d3ef3bcd5ae968c248d150c73c655c02b52f2
Rob Ashton <robashton@codeofrob.com>
2013-11-04 14:34:33 +0000

    Playing about with a new in-memory transaction manager

--- src/cravendb/scratch.clj
+++ src/cravendb/scratch.clj
@@ -34,5 +34,46 @@
 #_ (restart)
 
 
+;; This needs to work for
+  ;; Single writes via database->
+  ;; Multi writes via bulk->
+  ;; Multi writes via replication
 
+(defn create [db]
+  { :db db :tx-count (atom 0) :in-flight (atom {})})
+    
+(defn open [{:keys [in-flight db tx-count]}]
+  (let [txid (swap! tx-count inc)] 
+    (swap! in-flight #(assoc-in %1 [:transactions txid] 
+                      { :tx (s/ensure-transaction db)}))
+    txid))
+
+(defn write-request [txid id document metadata]
+  (fn [in]
+    ;; Should get the history from the tx
+    ;; Should compare that against what we have in memory
+    ;; Should set the known history for that id in memory
+    ;; Should increase the counter for that known history
+    ;; Should map this id to the transaction id so we know to remove when done
+    ))
+
+(defn clean-up [txid]
+  (fn [in]
+    ;; Should decrease the counter for any ids held by this transaction
+    ;; Should remove this transaction
+    ;; Should remove any ids that have a counter of 0
+    ))
+
+(defn add-document [{:keys [in-flight]} txid id document metadata]
+  (swap! in-flight (write-request txid :doc-add id document metadata)))
+
+(defn remove-document [{:keys [in-flight]} tx txid id metadata]
+  (swap! in-flight (write-request txid :doc-delete id nil metadata)))
+
+;; For calling once a transaction is actually committed
+(defn complete! [{:keys [in-flight]} txid]
+  ;; de-ref that specific id
+  ;; commit the transaction
+  ;; remove it from the in-flight register
+  (swap! in-flight (clean-up txid)))
 
