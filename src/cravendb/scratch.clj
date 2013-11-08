0996e10c2c0ac18ed25ce880d34929cbd7a48ca1
Rob Ashton <robashton@codeofrob.com>
2013-11-08 12:36:09 +0000

    Can create arbitrary streams of clojure objects...

--- src/cravendb/scratch.clj
+++ src/cravendb/scratch.clj
@@ -5,5 +5,17 @@
         [clojure.tools.logging :only (info debug error)]
         [clojure.core.async])
   (:import (java.io File File PushbackReader IOException FileNotFoundException ))
-  (:require [clojure.edn :as edn]))
+  (:require [clojure.edn :as edn]
+            [clojure.pprint :refer [pprint]]
+            ))
 
+#_(with-open [stream (db/stream-into "key")] 
+    (binding [*out* (clojure.java.io/writer stream)]
+      (pr 2)
+      (pr "hello")
+      (pr { :hello "world"})
+      (.flush *out*)))
+
+#_ (let [*out* ] 
+    
+    )
