858960a17a19c637aafea269f59da8420640e4a6
Rob Ashton <robashton@codeofrob.com>
2013-11-23 08:42:13 +0000

    Work on the public surface area so it can be documented

--- src/cravendb/clienttransaction.clj
+++ src/cravendb/clienttransaction.clj
@@ -1,5 +1,9 @@
 (ns cravendb.clienttransaction
-   (:require [cravendb.client :as client]))
+   (:require [cravendb.client :as client]
+             [cravendb.database :as db]))
+
+
+(defn mode [tx & _] (if (:href tx) :remote :embedded))
 
 (defn store-document [tx id data]
   (assoc-in tx [:cache id] data))
@@ -7,17 +11,30 @@
 (defn delete-document [tx id]
   (assoc-in tx [:cache id] :deleted))
 
-(defn load-document [{:keys [href] :as tx} id]
+(defmulti from-storage mode)
+(defmethod from-storage :remote [{:keys [href]} id]
+  (client/get-document href id))
+(defmethod from-storage :embedded [{:keys [instance]} id]
+  (db/load-document instance id))
+
+(defn load-document [tx id]
   (let [cached (get-in tx [:cache id])]
     (if (= cached :deleted) nil
-      (or cached (client/get-document href id)))))
+      (or cached (from-storage tx id)))))
 
-(defn commit! [{:keys [href] :as tx}]
-  (client/bulk-operation href
-    (into () 
-    (for [[k v] (:cache tx)]
+(defn package [cache]
+  (into () 
+    (for [[k v] cache]
       (if (= v :deleted)
         { :operation :docs-delete :id k }
-        { :operation :docs-put :id k :document v })))))
+        { :operation :docs-put :id k :document v }))))
+
+(defmulti commit! mode)
+(defmethod commit! :remote 
+  [{:keys [href] :as tx}]
+  (client/bulk-operation href (package (:cache tx))))
+(defmethod commit! :embedded 
+  [{:keys [instance] :as tx}]
+  (db/bulk instance (package (:cache tx))))
 
 (defn start [href] { :href href})
